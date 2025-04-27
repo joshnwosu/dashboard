fee7eb1be6cce2bf2f3c3b192c648b98ac68ec9d
joshua <joshuanwosu078@gmail.com>
2025-04-27 07:52:14 +0100

    bunch of updates

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -5,36 +5,45 @@ import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
 import { Button } from '@/components/ui/button';
+
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
 
 export function ModeToggle() {
   const { setTheme } = useTheme();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant='outline' size='icon'>
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant='outline'
+          size='icon'
+          className='relative'
+          aria-label='Open mode toggle'
+        >
           <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
           <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
           <span className='sr-only'>Toggle theme</span>
         </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align='end'>
-        <DropdownMenuItem onClick={() => setTheme('light')}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+      </PopoverTrigger>
+      <PopoverContent className='w-25 p-1' align='end'>
+        {['light', 'dark', 'system'].map((mode, index) => (
+          <div
+            key={index.toString()}
+            className='rounded-md px-3 py-2 text-sm transition-colors hover:bg-accent'
+          >
+            <div
+              onClick={() => setTheme(mode)}
+              className='relative flex items-start'
+            >
+              {mode}
+            </div>
+          </div>
+        ))}
+      </PopoverContent>
+    </Popover>
   );
 }
